9ef7a4ecc3a9ed89be268813aa100c856d8efeb6
ImaaValenzuela <imanolvalenzuela03@gmail.com>
2024-11-10 14:07:46 -0300

    Fixed and Optimizated Code

--- src/main/kotlin/domain/TicketElite.kt
+++ src/main/kotlin/domain/TicketElite.kt
@@ -6,12 +6,12 @@ import java.time.LocalTime
 class TicketElite : Trade() {
 
     override fun tradeTicket(event: Event): Double {
-        val commissionCalculator: (LocalTime) -> Double = { hour ->
-            val startRange = LocalTime.of(20, 0)
-            val endRange = LocalTime.of(23, 59)
-            if (!hour.isBefore(startRange) && !hour.isAfter(endRange)) 0.01 else 0.03
+        val commissionRate = if (!event.hour.isBefore(LocalTime.of(20, 0)) && !event.hour.isAfter(LocalTime.of(23, 59))) {
+            CommissionRates.PEAK_HOUR_COMMISSION
+        } else {
+            CommissionRates.OFF_PEAK_HOUR_COMMISSION
         }
-        val cost = event.price * (1 + commissionCalculator(event.hour))
+        val cost = event.price.times(1.plus(commissionRate)) //* (1 + commissionRate)
         println("The final price for Elite is: $cost")
         return cost
     }
--- src/main/kotlin/domain/TicketPro.kt
+++ src/main/kotlin/domain/TicketPro.kt
@@ -5,8 +5,7 @@ import data.Event
 class TicketPro : Trade() {
 
     override fun tradeTicket(event: Event): Double {
-        val commission = 0.02
-        val result = event.price * (1 + commission)
+        val result = event.price.times(1.plus(CommissionRates.PRO_COMMISSION)) // * (1 + CommissionRates.PRO_COMMISSION)
         println("The final price for Ticket Pro is: $result")
         return result
     }
